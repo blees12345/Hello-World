8c603ec8e0589ee08c8b7a2b589b9f74b16ee2ff
blees12345 <adriang1702@gmail.com>
2022-07-27 16:29:29 -0700

    added bootstrap modal

--- src/App.js
+++ src/App.js
@@ -2,39 +2,84 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Home/HomeContainer';
 import Body from './Home/Body';
 import VisitedCountries from './visitedCountries/visitedCountries';
-import Map from './map/map'
+import Map from './map/map';
 import './utilities/reset.css';
 import './App.css';
-// import HellowWorld from './Utilities/Hello-World.jpeg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useState } from 'react';
 
 function App() {
-  return (
-    <section className='main-container'>
-      <nav className='nav-container'>
-        <div className='logo-container-main'>
-          <Link to='/'>
-            {/* <img className='logo-container' src={} alt='' /> */}
-          </Link>
-        </div>
-        <ul className='link-container'>
-          <li className='link pic-lib-link'>
-            <Link to='/visitedCountries'>Visited Countries</Link>
-          </li>
-          <li className='link pic-lib-link'>
-            <Link to='/map'>Map</Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/body' element={<Body />} />
-          <Route path='/visitedcountries' element={<VisitedCountries />} />
-          <Route path='/map' element={<Map />} />
-        </Routes>
-      </main>
-    </section>
-  );
+	function Storage() {
+		// Similar to useState but first arg is key to the value in local storage.
+		const [name, setName] = useLocalStorage('name', 'Bob');
+		return (
+			<div>
+				<input
+					type='text'
+					placeholder='Enter your name'
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</div>
+		);
+	}
+	// Hook
+	function useLocalStorage(key, initialValue) {
+		const [storedValue, setStoredValue] = useState(() => {
+			if (typeof window === 'undefined') {
+				return initialValue;
+			}
+			try {
+				const item = window.localStorage.getItem(key);
+				return item ? JSON.parse(item) : initialValue;
+			} catch (error) {
+				console.log(error);
+				return initialValue;
+			}
+		});
+		const setValue = (value) => {
+			try {
+				const valueToStore =
+					value instanceof Function ? value(storedValue) : value;
+				setStoredValue(valueToStore);
+				if (typeof window !== 'undefined') {
+					window.localStorage.setItem(key, JSON.stringify(valueToStore));
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		return [storedValue, setValue];
+	}
+
+	return (
+		<section className='main-container'>
+			<nav className='nav-container'>
+				<div className='logo-container-main'>
+					<Link to='/'>
+						{/* <img className='logo-container' src={} alt='' /> */}
+					</Link>
+				</div>
+				<ul className='link-container'>
+					<li className='link pic-lib-link'>
+						<Link to='/visitedCountries'>Visited Countries</Link>
+					</li>
+					<li className='link pic-lib-link'>
+						<Link to='/map'>Map</Link>
+					</li>
+				</ul>
+			</nav>
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/body' element={<Body />} />
+					<Route path='/visitedcountries' element={<VisitedCountries />} />
+					<Route path='/map' element={<Map />} />
+				</Routes>
+			</main>
+		</section>
+	);
 }
 
 export default App;
--- src/map/map.js
+++ src/map/map.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-
+import  { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+const keys = 'AIzaSyDpJXpPXK9Y2YpDX0Xk_6Rp3bAo0TLnwOY';
 const containerStyle = {
-	width: '400px',
-	height: '400px',
+	width: '100vh',
+	height: '100vh',
 };
 
 const center = {
 	lat: -3.745,
 	lng: -38.523,
 };
+console.log(keys)
 
 function MyComponent() {
 	const { isLoaded } = useJsApiLoader({
 		id: 'google-map-script',
-		googleMapsApiKey: 'AIzaSyDpJXpPXK9Y2YpDX0Xk_6Rp3bAo0TLnwOY',
+		googleMapsApiKey: keys,
 	});
 
 	const [map, setMap] = React.useState(null);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	const onLoad = React.useCallback(function callback(map) {
 		const bounds = new window.google.maps.LatLngBounds(center);
@@ -30,18 +38,41 @@ function MyComponent() {
 	}, []);
 
 	return isLoaded ? (
-		<GoogleMap
-			mapContainerStyle={containerStyle}
-			center={center}
-			zoom={10}
-			onLoad={onLoad}
-			onUnmount={onUnmount}>
-			{/* Child components, such as markers, info windows, etc. */}
-			<></>
-		</GoogleMap>
+		<div>
+			<GoogleMap
+				mapContainerStyle={containerStyle}
+				center={center}
+				zoom={10}
+				onLoad={onLoad}
+				onUnmount={onUnmount}>
+				{/* Child components, such as markers, info windows, etc. */}
+			</GoogleMap>
+			<>
+				<Button variant='primary' onClick={handleShow}>
+					Launch demo modal
+				</Button>
+
+				<Modal show={show} onHide={handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Modal heading</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+					<Modal.Footer>
+						<Button variant='secondary' onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant='primary' onClick={handleClose}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</>
+		</div>
 	) : (
 		<></>
 	);
-}
+	
 
+	
+}
 export default React.memo(MyComponent);
