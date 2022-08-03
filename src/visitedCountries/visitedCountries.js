d4fee3b13a8af950d97337cc9c40c39d4fc034d6
blees12345 <adriang1702@gmail.com>
2022-08-03 09:42:23 -0700

    rebase

--- src/visitedCountries/visitedCountries.js
+++ src/visitedCountries/visitedCountries.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Geocode from 'react-geocode';
+<<<<<<< HEAD
 import './visitedCountries.css';
 
 function VisitedCountries() {
@@ -48,6 +49,50 @@ function VisitedCountries() {
             </div>
         </section>
     ) : null;
+=======
+function VisitedCountries() {
+	const [userData, setUserData] = useState([]);
+	console.log('userData', userData);
+	useEffect(() => {
+		function checkUserData() {
+			const item = JSON.parse(localStorage.getItem('mapMarker'));
+			console.log('item', item);
+			if (item) {
+				setUserData(item);
+			} else {
+				setUserData('Default Value');
+			}
+		}
+		checkUserData();
+	}, []);
+	console.log(userData);
+	const item = JSON.parse(localStorage.getItem('mapMarker'));
+	Geocode.setApiKey(process.env.REACT_APP_API_MAP);
+	const [address, setAddress] = useState([]);
+	async function getAddresses() {
+        const results = [];
+		for (let i = 0; i < item.length; i++) {
+            const response = await Geocode.fromLatLng(item[i].pos.lat, item[i].pos.lng)
+            results.push(response.results[0].formatted_address)
+		}
+        setAddress(results) 
+console.log(results)
+	}
+
+	useEffect(() => {
+		getAddresses()
+	}, []);
+
+	return address[0] ? (
+		
+			<div >
+				{address.map((address) => (
+					<div className='address'>{address}</div>
+				))}
+			</div>
+			) : null;
+	
+>>>>>>> e8c453f (added list of locations on markers, added locations info window)
 }
 
 export default VisitedCountries;
