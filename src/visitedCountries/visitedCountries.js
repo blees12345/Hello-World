4451aace5e808429d2bd1b99a666e3580e58fb19
blees12345 <adriang1702@gmail.com>
2022-08-02 17:24:22 -0700

    added list of locations on markers, added locations info window

--- src/visitedCountries/visitedCountries.js
+++ src/visitedCountries/visitedCountries.js
@@ -1,28 +1,47 @@
 import React, { useState, useEffect } from 'react';
-
+import Geocode from 'react-geocode';
 function VisitedCountries() {
-    const [userData, setUserData] = useState([]);
-    console.log('userData', userData);
-    useEffect(() => {
-        function checkUserData() {
-            const item = JSON.parse(localStorage.getItem('mapMarker'));
-            console.log('item', item);
-            if (item) {
-                setUserData(item);
-            } else {
-                setUserData('Default Value');
-            }
-        }
-        checkUserData();
-    }, []);
-    return userData[0] ? (
-        <>
-            <h2>Lat: {userData[0].pos.lat}</h2>
-            <h2>Long: {userData[0].pos.lng}</h2>
-        </>
-    ) : (
-        null
-    );
+	const [userData, setUserData] = useState([]);
+	console.log('userData', userData);
+	useEffect(() => {
+		function checkUserData() {
+			const item = JSON.parse(localStorage.getItem('mapMarker'));
+			console.log('item', item);
+			if (item) {
+				setUserData(item);
+			} else {
+				setUserData('Default Value');
+			}
+		}
+		checkUserData();
+	}, []);
+	console.log(userData);
+	const item = JSON.parse(localStorage.getItem('mapMarker'));
+	Geocode.setApiKey(process.env.REACT_APP_API_MAP);
+	const [address, setAddress] = useState([]);
+	async function getAddresses() {
+        const results = [];
+		for (let i = 0; i < item.length; i++) {
+            const response = await Geocode.fromLatLng(item[i].pos.lat, item[i].pos.lng)
+            results.push(response.results[0].formatted_address)
+		}
+        setAddress(results) 
+console.log(results)
+	}
+
+	useEffect(() => {
+		getAddresses()
+	}, []);
+
+	return address[0] ? (
+		
+			<div >
+				{address.map((address) => (
+					<div className='address'>{address}</div>
+				))}
+			</div>
+			) : null;
+	
 }
 
 export default VisitedCountries;
